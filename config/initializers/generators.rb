6db6d12b23988daa59003bc24fcc63c8f42669c6
great084 <great084@gmail.com>
2020-04-10 01:24:14 +0900

    Gemインストール＆初期設定

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+    g.skip_routes true
+    g.assets false
+    g.stylesheets false
+    g.helper false
+    g.template_engine :erb
+    g.test_framework false
+  end
