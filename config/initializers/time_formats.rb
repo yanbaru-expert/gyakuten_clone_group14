2aa8ec582132ff11a28e4d6cbd010a46ff750f78
健太 <kenta@mba.local>
2020-04-14 15:25:13 +0900

    質問ページの作成

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS[:date_jp] = '%Y年 %m月 %d日'
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'movies#index'
+  resources :questions, only: [:index]
 end
