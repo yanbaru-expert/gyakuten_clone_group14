22855835712ba21c9edac73a01d2e95ae4b0148c
健太 <kenta@mba.local>
2020-04-15 23:49:19 +0900

    質問の新規投稿機能の作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,16 @@
 class QuestionsController < ApplicationController
     def index
+        @question = Question.new 
         @questions = Question.all
+    end
+
+    def create
+        Question.create(question_params)
+        redirect_to questions_path
+    end
+
+    private
+    def question_params
+        params.require(:question).permit(:title,:detail)
     end
 end
