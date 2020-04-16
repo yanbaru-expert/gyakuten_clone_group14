271deb27a52f2abb2332db1db8f5a2a4d5a5f093
健太 <kenta@mba.local>
2020-04-16 11:42:49 +0900

    フラッシュメッセージが出るように修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,8 +5,12 @@ class QuestionsController < ApplicationController
     end
 
     def create
-        Question.create(question_params)
-        redirect_to questions_path
+        @question = Question.new(question_params)
+        if @question.save
+            redirect_to questions_path, notice: "質問を投稿しました"
+        else
+            redirect_to questions_path, alert: "質問を投稿できませんでした"
+        end
     end
 
     private
