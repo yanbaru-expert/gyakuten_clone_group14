fd495502257b61bdce613101f140d559cabc67d6
great084 <great084@gmail.com>
2020-04-18 15:12:53 +0900

    21 質問詳細画面にCSSを適用

--- db/migrate/20200417004958_create_solutions.rb
+++ db/migrate/20200417004958_create_solutions.rb
@@ -1,7 +1,6 @@
 class CreateSolutions < ActiveRecord::Migration[6.0]
   def change
     create_table :solutions do |t|
-      t.string :title
       t.text :detail
       t.references :question, null: false, foreign_key: true
 
