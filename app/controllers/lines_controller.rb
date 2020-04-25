972eb557b9b0cd07b8eb3d6ee38aa32ab4d84ed8
健太 <kenta@mba.local>
2020-04-25 14:06:27 +0900

    helperを用いてheaderとfooterのviewを場合分け

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,14 +1,10 @@
 class LinesController < ApplicationController
-    before_action :change_background_color
+  
     def index
         @lines = Line.all
     end
 
     def show
         @line = Line.find(params[:id])
-    end
-
-    def change_background_color
-        @background_color_class = "bg-success"
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,8 @@ module ApplicationHelper
 
     Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
   end
+
+  def background_color_class
+    (@line != nil || @lines != nil) ? "bg-success" : "bg-primary"
+  end
 end
