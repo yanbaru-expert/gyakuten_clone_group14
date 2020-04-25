0e8b1417fe5648e154864b61e5dab64c8b517b80
健太 <kenta@mba.local>
2020-04-25 11:32:26 +0900

    LINE@のヘッダーとフッターの色を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :background_color
+
+    def background_color
+        @background_color_class = "bg-primary"
+    end
 end
--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,9 +1,14 @@
 class LinesController < ApplicationController
+    before_action :change_background_color
     def index
         @lines = Line.all
     end
 
     def show
         @line = Line.find(params[:id])
+    end
+
+    def change_background_color
+        @background_color_class = "bg-success"
     end
 end
