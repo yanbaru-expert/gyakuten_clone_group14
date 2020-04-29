f74ce41769ea9a8158a6a9aa56813a9e6baddcae
daisuke8000 <daisuke12108000@gmail.com>
2020-04-29 15:28:34 +0900

    表示画面の微調整

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,12 +1,6 @@
 class TextsController < ApplicationController
   def index
     @texts = Text.all
-    @query = Article.ransack(params[:q])
-    @articles = @query.result.by_publish_time_desc
-
-    if params[:q].present?
-      render json:  @articles.select("title").map { |e| e.title  }.to_json
-    end
   end
 
   def show
