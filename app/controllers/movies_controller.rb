3725cd7b432b11791ed253a6b34e4d3e1d47f668
健太 <kenta@mba.local>
2020-05-06 11:09:35 +0900

    最低限の検索機能の完成

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,9 @@ class MoviesController < ApplicationController
     @movies = Movie.where(category: @search_category).page(params[:page])
   end
 
+  def search
+    @movies = Movie.where(category: @search_category).where('title iLIKE(?)', "%#{params[:keyword]}%").page(params[:page])
+    render json: @movies
+  end
+
 end
---
+++ app/javascript/packs/movies.js
@@ -0,0 +1,60 @@
+document.addEventListener('turbolinks:load', function() {
+
+    $("#search-movies").keyup(function(){
+
+        var val = $(this).val();
+
+        $.ajax({ 
+            url: '/movies/search', 
+            type: 'GET', 
+            data: ('keyword=' + val), 
+            processData: false, 
+            contentType: false, 
+            dataType: 'json' 
+        })
+
+        .done(function(data){
+            console.log(data)
+            if (data.length == 10){
+                $(".video-contents").removeClass("hidden")
+                $("nav").removeClass("hidden")
+                $(".searched-video").addClass("hidden")
+            }else{
+                $(".video-contents").addClass("hidden")
+                $("nav").addClass("hidden")
+                
+                 
+                $(data).each(function(i, video){ 
+                    var common = '<div class="video-contents searched-video">'
+                                +'<p class="video-title">'
+                                + video.title
+                                +'</p>'
+                                +'<div class="video-content">'
+                                    +'<iframe src='
+                                    + video.url
+                                    +' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+                                +'</div>'
+                            +'</div>'
+                    
+                    var option = '<a href='
+                                +  video.source_code_url 
+                                + ' class="video-source-link">ソースコード</a>'
+
+                           
+                    $('#movie').append(common)
+
+                    if(video.source_code_url != null){
+                        $('#movie').append(option)
+                    }
+                });
+ 
+            }
+       
+
+     
+
+        });
+
+    });
+
+});
