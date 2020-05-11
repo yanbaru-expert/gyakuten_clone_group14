c359b8a98d958caf9d38ee041b2f226b720c91f5
健太 <kenta@mba.local>
2020-05-11 16:11:57 +0900

    contextを追加

--- spec/models/solution_spec.rb
+++ spec/models/solution_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Solution, type: :model do
-    it "detailが空だと無効であること" do
-        answer = FactoryBot.build(:null_data_solution)
-        expect(answer.valid?).to eq(false)
+    context "detailが空のとき" do
+        it "validationエラーが起きる" do
+            answer = FactoryBot.build(:null_data_solution)
+            expect(answer.valid?).to eq(false)
+        end
     end
 
-    
-
-    it "detailが入力されていてquestionが紐づいていれば保存できること" do
-        answer = FactoryBot.build(:solution_with_question)
-        expect(answer.valid?).to eq(true)
+    context "detailが入力されていてquestionが紐づいているとき" do
+        it "validationエラーが起きない" do
+            answer = FactoryBot.build(:solution_with_question)
+            expect(answer.valid?).to eq(true)
+        end
     end
 end
