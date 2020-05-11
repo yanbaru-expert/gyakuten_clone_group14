88762ea6459394eb840e7af8dbeff2832039cdf5
健太 <kenta@mba.local>
2020-05-11 17:05:52 +0900

    FactoryBotで生成していたデータをletで書き換え

--- Gemfile
+++ Gemfile
@@ -22,7 +22,6 @@ gem 'rouge'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem "factory_bot_rails"
 end
 
 group :development do
--- spec/models/solution_spec.rb
+++ spec/models/solution_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Solution, type: :model do
+    let(:null_data_solution) { Solution.new(detail: "") }
     context "detailが空のとき" do
         it "validationエラーが起きる" do
-            answer = FactoryBot.build(:null_data_solution)
-            expect(answer.valid?).to eq(false)
+            expect(null_data_solution).not_to be_valid
         end
     end
 
+    let!(:question) {Question.create(title: "test_title",detail:"test_detail")}
+    let!(:solution_with_question) {Solution.new(detail: "test_detail",question_id: question.id)}
     context "detailが入力されていてquestionが紐づいているとき" do
         it "validationエラーが起きない" do
-            answer = FactoryBot.build(:solution_with_question)
-            expect(answer.valid?).to eq(true)
+            expect(solution_with_question).to be_valid
         end
     end
 end
