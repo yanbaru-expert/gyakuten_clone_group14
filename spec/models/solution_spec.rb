00553dffc85b281cdf56e4bd9e0b33d818eda826
健太 <kenta@mba.local>
2020-05-12 15:30:44 +0900

    エラーの原因が分かるようにテストコードを変更

--- spec/models/solution_spec.rb
+++ spec/models/solution_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
+require 'pry'
 RSpec.describe Solution, type: :model do
     describe "Solution_modelのテスト" do
+        
         context "detailが空の場合" do
             let(:detail_nil_solution){FactoryBot.build(:detail_nil_solution)}
             it "validationエラーが起こる" do
                 expect(detail_nil_solution).not_to be_valid
+                expect(detail_nil_solution.errors.full_messages).to include "詳細を入力してください"
             end
         end
         context "detailaが空でない場合" do
