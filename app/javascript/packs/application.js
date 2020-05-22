77662a8ac747ca63c89030c9ecf8539dfb817b6b
great084 <great084@gmail.com>
2020-05-22 10:31:37 +0900

    41 application.jsの記載方法を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,5 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
-import '@fortawesome/fontawesome-free/js/all'
+require("@fortawesome/fontawesome-free/js/all")
 
